fa79ae8eb2c8f3e5e6977492a84af00f4a956c32
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2019-07-05 19:41:59 +0300

    Implement traverseWord function

---
+++ src/main/scala/Automaton.scala
@@ -0,0 +1,64 @@
+import scala.annotation.tailrec
+
+case class Automaton(name: String,
+                     states: List[String], alphabet: List[Char],
+                     startState: String, finalStates: List[String],
+                     deltaFunction: Map[(String, Char), String]) {
+def isWordRecognized(word: String): Boolean = traverseWord(word) match {
+case Success(_) => true
+case Failure(_, _) => false
+case _ => sys.error("Unexpected return type of traverseWord. ")
+}
+
+def traverseWord(word: String): Checked[String, String] = {
+  val listOfLetters = word.toList
+  helper(startState, 0, "")
+
+  @tailrec
+  def helper(currState: String, currLetterIndex: Int, path: String): Checked[String, String] ={
+    val currLetter = listOfLetters(currLetterIndex)
+
+    //next letter exists
+    if(listOfLetters(currLetterIndex + 1) != '\0') {
+      if(isTransitionPossible(currState,currLetter)){
+        helper (findNextState(currState, currLetter), currLetterIndex + 1,
+                path ++ currState ++ currLetter.toString)
+      }
+      else{
+        return Failure(path, currState)
+      }
+    }
+    //there are no letters left
+    else{
+      if(finalStates.contains(currState)){
+        return Success(path ++ currState)
+      }
+      else{
+        return Failure(path ++ currState, currState)
+      }
+    }
+    }
+  }
+
+  def isTransitionPossible(fromState: String, withLetter: Char): Boolean ={
+    val possibleStates = deltaFunction.get((fromState, withLetter))
+
+    possibleStates match {
+      case Some(String) => true
+      case _ => false
+    }
+  }
+
+def findNextState(fromState:String, withLetter: Char): String = {
+  val possibleStates = deltaFunction.get((fromState, withLetter))
+
+  possibleStates match {
+    case Some(String) => String
+    case _ => sys.error("Wrong way calling function findNextState. " +
+                        "It must be called only when we are sure there is next state. ")
+  }
+
+}
+object Automaton{
+  def apply() = ???
+}
